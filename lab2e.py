e42ddf040479da67aa202dfd1c5ad437fd80c526
Syed Abdullah <sabdullah27@myseneca.ca>
2026-05-26 08:58:54 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Syed Abdullah
+# Author ID: sabdullah27
+# Date Created: 2026/05/25
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
