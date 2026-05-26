ce30d5402e6e3de5633ce151a4d7cc1263c4c8ca
Syed Abdullah <sabdullah27@myseneca.ca>
2026-05-26 09:04:17 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Syed Abdullah
+# Author ID: sabdullah27
+# Date Created: 2026/05/25
+
+import sys
+
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print (timer)
+    timer = timer - 1
+print('blast off!')
